a4c3ecb94a9334711937656d79a9cb863114a415
Jessie Guzman <jessieguzman777@gmail.com>
2022-11-01 18:02:39 -0500

    commit tree 77a7dca2a48054bb775ddebd83dbb1e185a509b7

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
-
-function scuberGreetingForFeet(feet) {
-  if (feet <= 400) {
-   return 'This one is on me!';
- } else if (feet > 2000 && feet < 2500) {
-   return 'I will gladly take your thirty bucks.';
- } else if (feet > 2500) {
-   return 'No can do.';
- }
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return "This one is on me!"
+   } else if (feet > 2000 && feet <= 2500){
+      return "I will gladly take your thirty bucks."
+   } else if (feet > 2500){
+      return "No can do."
+   }
 }
 
 
-function ternaryCheckCity(city) {
-  return (city === "NYC" ? "Ok, sounds good." : "No go.");
+function ternaryCheckCity(city){
+  return city === "NYC"?"Ok, sounds good.":"No go."
 }
 
-
-  // Write your code here!
-
-
-
-function switchOnCharmFromTip(tip) {
-  switch (tip) {
-    case 'generous': 
-      return ('Thank you so much.');
-      break;
-      case 'not as generous': 
-      return ('Thank you.');
-      break;  
-      default:
-      return 'Bye.'
-      break;
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default: 
+      return "Bye."
   }
 }
